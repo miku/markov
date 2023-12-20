51ca25f9ca7cc79cf5aab18c4ba18971550d9b2f
Martin Czygan <martin.czygan@gmail.com>
2023-12-20 01:19:35 +0100

    update notes

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # markov
-Codewalk Markov Chain Algorithm
+
+Codewalk: [Markov chain algorithm](https://go.dev/doc%2Fcodewalk%2Fmarkov.go)
+
+[![](static/scan010_text-0.png)](https://archive.org/details/ComputerRecreationsMarkovChainer)
+
+
+
---
+++ markov.go
@@ -0,0 +1,12 @@
+package main
+
+/*
+
+Generating random text: A Markov chain algorithm
+
+Based on the program presented in the "Design and Implementation" chapter of
+The Practice of Programming (Kernighan and Pike, Addison-Wesley 1999). See also
+Computer Recreations, Scientific American 260, 122-125 (1989)
+
+
+*/
