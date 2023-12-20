5121b3a9bb9faed061b63c130b2d73c57858eb75
Martin Czygan <martin.czygan@gmail.com>
2023-12-20 11:44:15 +0100

    sketch

--- markov.go
+++ markov.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strings"
+)
+
 /*
 
 Generating random text: A Markov chain algorithm
@@ -8,5 +15,79 @@ Based on the program presented in the "Design and Implementation" chapter of
 The Practice of Programming (Kernighan and Pike, Addison-Wesley 1999). See also
 Computer Recreations, Scientific American 260, 122-125 (1989)
 
+A Markov chain algorithm generates text by creating a statistical model of the
+potential textual suffixes for a given prefix. Consider this text:
+
+	I am not a number! I am a free man.
+
+Our Markov chain algorithm would arrange this text into this set of prefixes
+and suffixes, or "chain": (This table assumes a prefix length of two words.)
+
+	Prefix       Suffix
+
+	"" ""        I
+	"" I         am
+	I am         a
+	I am         not
+	a free       man!
+	am a         free
+	am not       a
+	a number!    I
+	number! I    am
+	not a        number!
+
+To generate text using this table we select an initial prefix ("I am", for
+example), choose one of the suffixes associated with that prefix at random with
+probability determined by the input statistics ("a"), and the then create a new
+prefix by removing the first word from the prefix and appending the suffix
+(making the new prefix "am a"). Repeat this process until we can't find any
+suffixes for the current prefix or we exceed the word limit. (The word limit is
+necessary as the chain table may contain cycles.)
+
+Our version of this program reads text from standard inout, parsing it into a
+Markov chain, and writes generated text to standard output. The prefix and
+output lengths can be specified using the -prefix and -words flags on the
+command-line.
 
 */
+
+// Prefix is a Markov chain prefix of one or more words.
+type Prefix []string
+
+// String returns the Prefix as a string (for use as a map key).
+func (p Prefix) String() string {
+	return strings.Join(p, "")
+}
+
+// Shift removes the first word from the Prefix and appends the given word.
+
+func (p Prefix) Shift(word string) {
+	copy(p, p[1:])
+	p[len(p)-1] = word
+}
+
+// Chain contains a map ("chain") of prefixes to a list of suffixes. A prefix
+// is a string of prefixLen words joined with spaces. A suffix is a single
+// word. A prefix can have multiple suffixes.
+type Chain struct {
+	chain     map[string][]string
+	prefixLen int
+}
+
+func NewChain(prefixLen int) *Chain {
+	return &Chain{make(map[string][]string), prefixLen}
+}
+
+func (c *Chain) Build(r io.Reader) {
+	br := bufio.NewReader(r)
+	p := make(Prefix, c.prefixLen)
+	for {
+		var s string
+		if _, err := fmt.Scanf(br, &s); err != nil {
+			break
+		}
+		key := p.String()
+		c.chain[key] = append(c.chain[key], s)
+		p.Shift(s)
+	}
+}
